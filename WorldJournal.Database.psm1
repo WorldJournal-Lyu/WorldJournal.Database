d14cfc82134a8afb302ed7f85dea1c4a76f5b089
WorldJournal-Lyu <lyu@worldjournal.com>
2018-06-27 11:03:03 -0400

    Add Get-WJDatabase function

--- WorldJournal.Database.psm1
+++ WorldJournal.Database.psm1
@@ -7,6 +7,64 @@ WorldJournal.Database.psm1
 #>
 
 Add-Type -Path "C:\Oracle\instantclient_10_2\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+$xmlPath = (Split-Path (Split-Path (Split-Path ($MyInvocation.MyCommand.Path) -Parent) -Parent) -Parent)+"\_DoNotRepository\"+(($MyInvocation.MyCommand.Name) -replace '.psm1', '.xml')
+[xml]$xml = Get-Content $xmlPath -Encoding UTF8
+
+
+
+
+function Get-WJDatabase() {
+    [CmdletBinding()]
+    Param ()
+    DynamicParam {
+
+        $paramDictionary = New-Object -Type System.Management.Automation.RuntimeDefinedParameterDictionary
+
+        $attributes = New-Object System.Management.Automation.ParameterAttribute
+        $attributes.Mandatory = $false
+        $attributes.ParameterSetName = '__AllParameterSets'
+        $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $attributeCollection.Add($attributes)
+        $values = $xml.Root.Database.Name | Select-Object -Unique
+        $validateSet = New-Object System.Management.Automation.ValidateSetAttribute($values)    
+        $attributeCollection.Add($validateSet)
+        $dynamicParam = New-Object -Type System.Management.Automation.RuntimeDefinedParameter(
+            "Name", [string], $attributeCollection
+        )
+
+        $paramDictionary.Add("Name", $dynamicParam)
+
+        return $paramDictionary
+
+    }
+
+    begin {}
+    process {
+
+        $Name = $PSBoundParameters.Name
+    
+        $whereArray = @()
+        if ($Name -ne $null) { $whereArray += '$_.Name -eq $Name' }
+        $whereString = $whereArray -Join " -and "  
+        $whereBlock = [scriptblock]::Create( $whereString )
+
+        if ($PSBoundParameters.Count -ne 0) {
+            $xml.Root.Database | Where-Object -FilterScript $whereBlock | Select-Object Name, Username, Password, Datasource
+        }
+        else {
+            $xml.Root.Database | Select-Object Name, Username, Password, Datasource
+        }
+
+    }
+    end {}
+}
+
+
+
+
+
+
+
 
 Function Query-Database{
 
